e1638f8371f700339bd942c2847fa12fc980772b
My Name <tadzhibaevaelnura@gmail.com>
2022-10-24 21:03:52 +0300

    Initial commit

---
+++ src/main/java/peaksoft/service/CourseService.java
@@ -0,0 +1,15 @@
+package peaksoft.service;
+
+import peaksoft.model.Course;
+
+import java.util.List;
+
+public interface CourseService {
+
+    List<Course> getAllCourse();
+
+    void addCourse(Course course, Long companyId);
+    Course getCourseById(Long id);
+    void updateCourse(Course course);
+    void deleteCourse(Course course);
+}
